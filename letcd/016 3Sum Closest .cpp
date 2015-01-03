bcc511de3d2e196823b56ab1c85de05b649cd888
huoyao <huoyaotl123@126.com>
2015-01-03 23:23:35 +0800

    016 3sum closest --own version

---
+++ letcd/016 3Sum Closest .cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+  int threeSumClosest(vector<int> &num, int target) {
+    sort(num.begin(), num.end());
+    int maxre = INT_MAX;
+    for (int i = 0; i < (int)(num.size() - 2); ++i)  //mark
+    {
+      if (i>0 && num[i] == num[i - 1]) continue;
+      int lidx = i + 1, ridx = num.size() - 1, sum = target - num[i], sumtmp;
+      while (lidx < ridx)
+      {
+        sumtmp = num[lidx] + num[ridx]-sum;
+        if (sumtmp == 0)
+          return target;
+        if (abs(sumtmp)<abs(maxre))
+        {
+          maxre = sumtmp;
+          //while (num[lidx + 1] == num[lidx] && lidx + 1 < ridx-1) ++lidx;
+          //while (num[ridx - 1] == num[ridx] && ridx - 1 > lidx+1) --ridx;
+        }
+        if (sumtmp > 0)
+          --ridx;
+        else
+          ++lidx;
+      }
+    }
+    return target + maxre;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> vecnum = { 13, 2, 0, -14, -20, 19, 8, -5, -13, -3, 20, 15, 20, 5, 13, 14, -17, -7, 12, -6, 0, 20, -19, -1, -15, -2, 8, -2, -9, 13, 0, -3, -18, -9, -9, -19, 17, -14, -19, -4, -16, 2, 0, 9, 5, -7, -4, 20, 18, 9, 0, 12, -1, 10, -17, -11, 16, -13, -14, -3, 0, 2, -18, 2, 8, 20, -15, 3, -13, -12, -2, -19, 11, 11, -10, 1, 1, -10, -2, 12, 0, 17, -19, -7, 8, -19, -17, 5, -5, -10, 8, 0, -12, 4, 19, 2, 0, 12, 14, -9, 15, 7, 0, -16, -5, 16, -12, 0, 2, -16, 14, 18, 12, 13, 5, 0, 5, 6 };
+  int target = -59;
+  int res = slu.threeSumClosest(vecnum, target);
+  cout << res << endl;
+  system("pause");
+}
