83b90c71cbdd66bdc16e4636d2399fc6d580bc9c
huoyao <huoyaotl123@126.com>
2015-01-04 22:08:37 +0800

    021 merge two sorted lists

---
+++ letcd/021 merge two sorted lists.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    ListNode *pt1 = l1, *pt2 = l2, *head = NULL, *pt = new ListNode(0);
+    while (pt1 && pt2)
+    {
+      if (pt1->val < pt2->val) {
+        pt->next = pt1;
+        pt1 = pt1->next;
+      }
+      else
+      {
+        pt->next = pt2;
+        pt2 = pt2->next;
+      }
+      if (!head) head = pt;
+      pt = pt->next;
+    }
+    if (!pt1) pt->next = pt2;
+    if (!pt2) pt->next = pt1;
+    pt = head;
+    head = head->next;
+    pt->next = NULL;
+    delete(pt);
+    return head;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  ListNode *l1 = new ListNode(2), *l2 = new ListNode(1);
+  ListNode *res = slu.mergeTwoLists(l1, l2);
+  while (res)
+  {
+    cout << res->val << endl;
+    res = res->next;
+  }
+  system("pause");
+}
+
