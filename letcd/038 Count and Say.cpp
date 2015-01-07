68884f1b135c518fa940d93419da590b79220778
huoyao <huoyaotl123@126.com>
2015-01-08 00:14:02 +0800

    038 Count and Say  --own version

---
+++ letcd/038 Count and Say.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string convert(const string &str)
+{
+  string st;
+  int len = str.length();
+  for (int i = 0; i < len; ++i)
+  {
+    int cnt = i;
+    while (i + 1 < len && str[i] == str[i + 1])++i;
+    cnt = i - cnt + 1;
+    st += ('0'+cnt);
+    st += str[i];
+  }
+  return st;
+}
+
+class Solution {
+public:
+  string countAndSay(int n) {
+    string str = "1";
+    while (--n)
+    {
+      str = convert(str);
+    }
+    return str;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int target = 6;
+  string res = slu.countAndSay(target);
+  cout << res << endl;
+  system("pause");
+}
