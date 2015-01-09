806433806886e62c9180db830155ed1c2d63665f
huoyao <huoyaotl123@126.com>
2015-01-09 20:56:41 +0800

    046 Permutations  --own version good

---
+++ letcd/046 Permutations.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void find(const vector<int> &num,vector<int> &rec,vector<bool> &notuse,vector<vector<int> > &res)
+{
+  if (rec.size() == num.size())
+  {
+    res.push_back(rec);
+    return;
+  }
+  for (int i = 0; i < num.size(); ++i)
+  {
+    if (i != 0 && num[i] == num[i - 1]) continue;
+    if (!notuse[i]) continue;
+    rec.push_back(num[i]);
+    notuse[i] = false;
+    find(num,rec,notuse,res);
+    rec.pop_back();
+    notuse[i] = true;
+  }
+}
+
+class Solution {
+public:
+  vector<vector<int> > permute(vector<int> &num) {
+    vector<int> record;
+    vector<vector<int> > res;
+    if (num.size() <= 1) { res.push_back(num); return res; }
+    sort(num.begin(), num.end());
+    vector<bool> notused(num.size(),true);
+    find(num,record,notused,res);
+    return res;
+  } 
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> input = { 1,2,3 };
+  vector<vector<int> > res = slu.permute(input);
+  for (auto &t : res)
+  {
+    for (auto &k : t)
+      cout << k << "  ";
+    cout << endl;
+  }
+  system("pause");
+}
