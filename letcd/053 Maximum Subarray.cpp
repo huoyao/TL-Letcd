e1fafc0d9cacc7df265a866b69dfcb21103343aa
huoyao <huoyaotl123@126.com>
2015-01-10 22:15:57 +0800

    053 Maximum Subarray  --own version good, solve so fast

---
+++ letcd/053 Maximum Subarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int maxSubArray(int A[], int n) {
+    int maxval = A[0], val = A[0];
+    for (int i = 1; i < n; ++i)
+    {
+      if (val <= 0)
+        val = A[i];
+      else
+        val += A[i];
+      if (val>maxval) maxval = val;
+    }
+    return maxval;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int input[] = { -2, -7, -11, -1 };
+  int target = 4;
+  int res = slu.maxSubArray(input, target);
+  cout << res << endl;
+  system("pause");
+}
