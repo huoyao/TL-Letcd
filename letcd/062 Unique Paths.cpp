28ba92987d61d6c0643db5cdc8900506b293f2c1
huoyao <huoyaotl123@126.com>
2015-01-11 22:18:14 +0800

    062 Unique Paths  --web version   --good

---
+++ letcd/062 Unique Paths.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+private:
+  int res;
+public:
+  int uniquePaths(int m, int n) {
+    vector<vector<int> > vec(m, vector<int>(n, 1));
+    for (int i = 1; i < m; ++i)
+      for (int j = 1; j < n; ++j)
+       vec[i][j] = vec[i - 1][j] + vec[i][j - 1];
+    return vec[m - 1][n - 1];
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int input = 23;
+  int target = 12;
+  int res = slu.uniquePaths(input, target);
+  cout << res << endl;
+  system("pause");
+}
