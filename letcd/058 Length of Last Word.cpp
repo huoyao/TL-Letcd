c1f7f17ce2649011aabe65e3d6254434b9b39100
huoyao <huoyaotl123@126.com>
2015-01-11 19:58:31 +0800

    058 Length of Last Word  --own version

---
+++ letcd/058 Length of Last Word.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int lengthOfLastWord(const char *s) {
+    int idx = 0, cnt = 0;
+    while (s[idx] == ' ') ++idx;
+    if (s[idx] == '\0') return 0;
+    while (s[idx] != '\0')
+    {
+      cnt = 0;
+      while (isalpha(s[idx])) { ++cnt; ++idx; }
+      while (s[idx] == ' ') ++idx; 
+    }
+    return cnt;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  char input[] = "go to hell god bless you";
+  int target = 9;
+  int res = slu.lengthOfLastWord(input);
+  cout <<res<< endl;
+  system("pause");
+}
