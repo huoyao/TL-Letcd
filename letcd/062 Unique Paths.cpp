5782be578d0c74a7fde4f042d6c8aa0d9c5ccd97
huoyao <huoyaotl123@126.com>
2015-01-11 22:20:14 +0800

    062 Unique Paths --web version --better

--- letcd/062 Unique Paths.cpp
+++ letcd/062 Unique Paths.cpp
@@ -8,11 +8,11 @@ private:
   int res;
 public:
   int uniquePaths(int m, int n) {
-    vector<vector<int> > vec(m, vector<int>(n, 1));
+    vector<int> vec(n, 1);
     for (int i = 1; i < m; ++i)
       for (int j = 1; j < n; ++j)
-       vec[i][j] = vec[i - 1][j] + vec[i][j - 1];
-    return vec[m - 1][n - 1];
+       vec[j]+=vec[j - 1];
+    return vec[n-1];
   }
 };
 
