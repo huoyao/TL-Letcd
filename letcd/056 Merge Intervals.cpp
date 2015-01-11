270dc66df59b38e57acd1280d35e1bde5a43284e
huoyao <huoyaotl123@126.com>
2015-01-11 19:38:31 +0800

    057 Insert Interval  --own version

---
+++ letcd/056 Merge Intervals.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Definition for an interval.
+struct Interval {
+  int start;
+  int end;
+  Interval() : start(0), end(0) {}
+  Interval(int s, int e) : start(s), end(e) {}
+};
+
+bool cmp(const Interval &t1,const Interval &t2)
+{
+  if (t1.start == t2.start)
+    return t1.end < t2.end;
+  return t1.start < t2.start;
+}
+
+class Solution {
+public:
+  vector<Interval> merge(vector<Interval> &intervals) {
+    vector<Interval> res;
+    if (intervals.size() <= 0) return res;
+    sort(intervals.begin(),intervals.end(),cmp);
+    Interval tmp(intervals[0].start,intervals[0].end);
+    int i = 1;
+    for (; i < intervals.size(); ++i)
+    {
+      if (intervals[i].start <= tmp.end)
+        tmp.end = max(tmp.end,intervals[i].end);
+      else
+      {
+        res.push_back(tmp);
+        tmp = intervals[i];
+      }
+    }
+    res.push_back(tmp);
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<Interval> input = { Interval(1,2),Interval(2,3),Interval(4,5)};
+  vector<Interval> res = slu.merge(input);
+  for (auto &t : res)
+    cout << t.start << "  "<<t.end<<endl;
+  system("pause");
+}
---
+++ letcd/057 Insert Interval.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Definition for an interval.
+struct Interval {
+  int start;
+  int end;
+  Interval() : start(0), end(0) {}
+  Interval(int s, int e) : start(s), end(e) {}
+};
+
+class Solution {
+public:
+  vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+    vector<Interval> res;
+    if (intervals.size() <= 0)  //mark
+    {
+      res.push_back(newInterval); 
+      return res;
+    }
+    int lval = newInterval.start, rval = newInterval.end,len=intervals.size(),idx=0;
+    while (idx < len && intervals[idx].end < lval) res.push_back(intervals[idx++]);
+    if (idx == len)
+    {
+      res.push_back(newInterval);
+      return res;
+    }
+    Interval tmp = intervals[idx];
+    while (idx < len && intervals[idx].start <= rval) ++idx;
+    tmp.end = max(intervals[idx-1].end,rval);
+    tmp.start = min(tmp.start,lval);  //mark
+    res.push_back(tmp);
+    while (idx < len) res.push_back(intervals[idx++]);
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<Interval> input = { Interval(1, 2), Interval(4, 7), Interval(10, 15) };
+  Interval target(5,12);
+  vector<Interval> res = slu.insert(input,target);
+  for (auto &t : res)
+    cout << t.start << "  " << t.end << endl;
+  system("pause");
+}
