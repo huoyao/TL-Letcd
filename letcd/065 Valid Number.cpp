5d59f6ee8efc61a1b715305da26c4a60754e3740
huoyao <huoyaotl123@126.com>
2015-01-12 20:11:23 +0800

    065 Valid Number  --web version

---
+++ letcd/065 Valid Number.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  bool isNumber(const char *s) {
+    bool has_exp = false, has_space = false, has_num = false,has_dot=false;
+    while (*s == ' ') ++s;
+    while (*s == '+' || *s == '-') ++s;
+    while (*s != '\0')
+    {
+      if (isdigit(*s))
+      {
+        if (has_space) return false;
+        else has_num = true;
+      }
+      else if (*s == ' ')
+        has_space = true;
+      else if (*s == '.')
+      {
+        if (!has_dot && !has_space && !has_exp) has_dot = true;
+        else return false;
+      }
+      else if (*s == 'e')
+      {
+        if (!has_exp && !has_space && has_num)
+        {
+          has_exp = true;
+          has_num = false;
+          if (*(s + 1) == '-' || *(s + 1) == '+') ++s;
+        }
+        else return false;
+      }
+      else return false;
+      ++s;
+    }
+    return has_num;  //mark
+  }
+};
+
+int main()
+{
+  Solution slu;
+  char input[] = "   0.3452  ";
+  bool res = slu.isNumber(input);
+  cout << res<<endl;
+  system("pause");
+}
