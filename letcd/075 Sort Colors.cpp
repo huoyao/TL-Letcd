9ed3c6daaa27654096a7e7479c8c9b892db31d9e
huoyao <huoyaotl123@126.com>
2015-01-13 23:16:00 +0800

    075 Sort Colors  --web version

---
+++ letcd/075 Sort Colors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  void sortColors(int A[], int n) {
+    if (!A || n < 2) return;
+    int redidx = 0, blueidx = n - 1;
+    for (int i = 0; i <= blueidx; ++i)
+    {
+      if (A[i] == 0)
+        swap(A[redidx++], A[i]);
+      else if (A[i] == 2)
+        swap(A[blueidx--],A[i--]);
+    }
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int  input[] = { 1,2,0 };
+  int target = 3;
+  slu.sortColors(input, target);
+  for (auto &t : input)
+    cout << t << "  ";
+  cout << endl;
+  system("pause");
+}
