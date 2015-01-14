77408fafe56692564eb4d0779599b707dbc91ac8
huoyao <huoyaotl123@126.com>
2015-01-14 19:57:00 +0800

    076 Minimum Window Substring  --web version

---
+++ letcd/076 Minimum Window Substring.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  string minWindow(string S, string T) {
+    int slen = S.length(), tlen = T.length(),start,end,minwindow=slen+1,cnt=0;
+    string res = "";
+    if (slen == 0 || tlen == 0) return res;
+    vector<int> needstr(256, 0), hasstr(256,0);
+    for (int i = 0; i < tlen; ++i)
+      ++needstr[T[i]];
+    for (start = 0, end = 0; end < slen; ++end)
+    {
+      char ch = S[end];
+      if (needstr[ch] == 0) continue;
+      ++hasstr[ch];
+      if (hasstr[ch] <= needstr[ch]) ++cnt;
+      if (cnt == tlen)
+      {
+        ch = S[start];
+        while (needstr[ch] == 0 || hasstr[ch]>needstr[ch])
+        {
+          if (hasstr[ch] > needstr[ch]) --hasstr[ch];
+          ++start;
+          ch = S[start];
+        }
+        int len = end - start + 1;
+        if (len < minwindow)
+        {
+          minwindow = len;
+          res = S.substr(start,len);
+        }
+      }
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  string input = "abcfjdskadsadsa";
+  string target = "abcd";
+  string res = slu.minWindow(input, target);
+  cout << res<<endl;
+  system("pause");
+}
