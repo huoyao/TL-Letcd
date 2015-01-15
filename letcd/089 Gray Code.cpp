abbd8d87a6d717c12bf2dae5d953e5a5a33796f4
huoyao <huoyaotl123@126.com>
2015-01-15 23:23:53 +0800

    089 Gray Code  --web version

---
+++ letcd/089 Gray Code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  vector<int> grayCode(int n) {
+    int num = 1 << n,i=0;
+    vector<int> res;
+    while (i < num)res.push_back((i>>1)^(i++));
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int input = 2;
+  vector<int> res = slu.grayCode(input);
+  for (auto &t : res)
+    cout << t << "  ";
+  cout << endl;
+  system("pause");
+}
