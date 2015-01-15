93c6e0482b219c8443b0be6a382c417c049cd323
huoyao <huoyaotl123@126.com>
2015-01-15 22:27:48 +0800

    087 Scramble String  --web version --better

--- letcd/087 Scramble String.cpp
+++ letcd/087 Scramble String.cpp
@@ -7,30 +7,20 @@ class Solution {
 public:
   bool isScramble(string s1, string s2) {
     if (s1.length() != s2.length()) return false;
+    if (s1 == s2) return true;
     int len = s1.length();
     vector<int> vecch(26,0);
     for (int i = 0; i < len; ++i){ ++vecch[s1[i] - 'a']; --vecch[s2[i] - 'a']; }
     for (int i = 0; i < 26; ++i) if (vecch[i] != 0) return false;
-    vector<vector<vector<bool> > > dp(len, vector<vector<bool> >(len, vector<bool>(len+1, false)));
-    for (int k = 1; k <= len; ++k)
+    bool res = false;
+    for (int t = 1; t < len; ++t)
     {
-      for (int i = 0; i <= len - k; ++i)
-      {
-        for (int j = 0; j <= len - k; ++j)
-        {
-          if (k == 1)
-            dp[i][j][k] = s1[i] == s2[j];
-          else
-          {
-            for (int t = 1; t < k; ++t)
-            {
-              if (dp[i][j][t] && dp[i + t][j + t][k - t] || dp[i][j + k - t][t] && dp[i + t][j][k - t]){ dp[i][j][k] = true; break; }
-            }
-          }
-        }
-      }
+      res = isScramble(s1.substr(0, t), s2.substr(0, t)) && isScramble(s1.substr(t),s2.substr(t));
+      if (res) return res;
+      res = isScramble(s1.substr(0, t), s2.substr(len - t)) && isScramble(s1.substr(t), s2.substr(0, len-t));
+      if (res) return res;
     }
-    return dp[0][0][len];
+    return res;
   }
 };
 
