77852c0bcacac63a130578a75b27c06f0426a970
huoyao <huoyaotl123@126.com>
2015-01-16 20:06:16 +0800

    091 Decode Ways  --web version

--- letcd/091 Decode Ways.cpp
+++ letcd/091 Decode Ways.cpp
@@ -4,30 +4,28 @@
 #include <string>
 using namespace std;
 
-void decode(string &s, const int idx, const int ctt, const int &len, int &cnt)
-{
-  if (idx + ctt >= len)
-  {
-    if (idx + ctt == len) ++ cnt;
-    return;
-  }
-  int tag = 0;
-  if (ctt == 1) tag = s[idx] - '0';
-  else
-    tag = (s[idx] - '0') * 10 + s[idx + 1] - '0';
-  if (tag<1 || tag>26) return;
-  decode(s, idx+ctt, 1, len, cnt);
-  decode(s,idx+ctt,2,len,cnt);
-}
-
 class Solution {
 public:
   int numDecodings(string s) {
-    int cnt = 0;
-    if (s.length() == 0) return cnt;
-    decode(s,0,1,s.length(),cnt);
-    decode(s,0,2,s.length(),cnt);
-    return cnt;
+    int len = s.length();
+    if (len == 0) return 0;
+    vector<int> dp(len, 0);
+    for (int i = 0; i < len; ++i)
+    {
+      if (s[i] >= '1' && s[i] <= '9')
+      {
+        if (i == 0) dp[i] = 1;
+        else dp[i] += dp[i - 1];
+      }
+      if (i == 0) continue;
+      string tmp(s, i - 1, 2);
+      if (tmp >= "10" && tmp <= "26")
+      {
+        if (i > 1) dp[i] += dp[i - 2];
+        else dp[i] += 1;
+      }
+    }
+    return dp[len-1];
   }
 };
 
@@ -36,6 +34,6 @@ int main()
   Solution slu;
   string input = "1787897759966261825913315262377298132516969578441236833255596967132573482281598412163216914566534565";
   int res = slu.numDecodings(input);
-  cout << res<<endl;
+  cout << res << endl;
   system("pause");
 }
