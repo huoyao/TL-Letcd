0cb49f82750e2c0b00b1279a8b10868d471783ef
huoyao <huoyaotl123@126.com>
2015-01-18 22:21:07 +0800

    102 Binary Tree Level Order Traversal  --own version

---
+++ letcd/102 Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Definition for binary tree
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+  vector<vector<int> > res;
+public:
+  void levelTra(TreeNode *rt,int level)
+  {
+    if (!rt) return;
+    if (level == res.size())
+    {
+      vector<int> v;
+      res.push_back(v);
+    }
+    res[level].push_back(rt->val);
+    levelTra(rt->left,level+1);
+    levelTra(rt->right,level+1);
+  }
+  vector<vector<int> > levelOrder(TreeNode *root) {
+    levelTra(root,0);
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  TreeNode *h = new TreeNode(2);
+  h->left = new TreeNode(3);
+  h->right = new TreeNode(1);
+  vector<vector<int> > res = slu.levelOrder(h);
+  for (auto &t : res)
+  {
+    for (auto &k : t)
+      cout << k << "  ";
+    cout << endl;
+  }
+  system("pause");
+}
