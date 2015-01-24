97fa98143809089c853ad9c8aa87c8c75724e6a3
huoyao <huoyaotl123@126.com>
2015-01-24 20:19:03 +0800

    134 Gas Station  --web version

---
+++ letcd/134 Gas Station.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+    if (gas.size() == 0 || cost.size() == 0 || gas.size() != cost.size()) return -1;
+    int all = 0, tempsum = 0, beg = 0;
+    for (int i = 0; i < gas.size(); ++i)
+    {
+      all += gas[i] - cost[i];
+      if (tempsum < 0)
+      {
+        tempsum = gas[i] - cost[i];
+        beg = i;
+      }
+      else
+        tempsum += gas[i] - cost[i];
+    }
+    return all >= 0 ? beg : -1;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> input = { 2, 7, 11, 15 }, target = { 2, 5, 7, 11 };
+  int res = slu.canCompleteCircuit(input, target);
+  cout << res << endl;
+  system("pause");
+}
