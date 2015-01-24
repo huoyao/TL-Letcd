ba7339c3ea261d2be9b58cd9d0a33eed14298d03
huoyao <huoyaotl123@126.com>
2015-01-24 20:31:15 +0800

    135 Cindy  --web version

---
+++ letcd/135 Cindy.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int candy(vector<int> &ratings) {
+    if (ratings.size() == 0) return 0;
+    int cindynum = 1;
+    vector<int> cindy(ratings.size(),1);
+    for (int i = 1; i < ratings.size(); ++i)
+    {
+      if (ratings[i]>ratings[i - 1])
+        cindy[i] = max(++cindynum, cindy[i]);
+      else
+        cindynum = 1;
+    }
+    cindynum = 1;
+    for (int i = ratings.size() - 2; i >= 0; --i)
+    {
+      if (ratings[i] > ratings[i + 1])
+        cindy[i] = max(++cindynum, cindy[i]);
+      else
+        cindynum = 1;
+    }
+    cindynum = 0;
+    for (int i = 0; i < cindy.size(); ++i)
+      cindynum += cindy[i];
+    return cindynum;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> input = { 3,2, 7, 11, 15 };
+  int res = slu.candy(input);
+  cout << res<<endl;
+  system("pause");
+}
