dd4a40af3419f2e61ab16d8a70f21c34a5c3a9fd
huoyao <huoyaotl123@126.com>
2015-01-27 23:17:19 +0800

    160 Intersection of Two Linked Lists  --own version

---
+++ letcd/160 Intersection of Two Linked Lists.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if (!headA || !headB) return NULL;
+    int lena = 0, lenb = 0;
+    ListNode *pa = headA, *pb = headB;
+    while (pa) { ++lena; pa = pa->next; }
+    while (pb) { ++lenb; pb = pb->next; }
+    int ex = abs(lena - lenb);
+    pa = headA;
+    pb = headB;
+    if (lena > lenb)
+      while (ex--) pa = pa->next;
+    if (lenb > lena)
+      while (ex--) pb = pb->next;
+    while (pa && pa->val != pb->val) { pa = pa->next; pb = pb->next; }
+    return pa;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  ListNode *h1 = new ListNode(1);
+  ListNode *h2 = new ListNode(2);
+  ListNode *h3 = new ListNode(3);
+  h1->next = h3;
+  h2->next = h3;
+  ListNode *res = slu.getIntersectionNode(h1, h2);
+  cout << res->val << endl;
+  system("pause");
+  return 0;
+}
