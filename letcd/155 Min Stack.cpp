943725e221c43d423d003abfebf50d541eb6a158
huoyao <huoyaotl123@126.com>
2015-01-27 21:53:15 +0800

    155 Min Stack  --own version

---
+++ letcd/155 Min Stack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+class MinStack {
+private:
+  stack<int> stk, minstk;
+public:
+  void push(int x) {
+    stk.push(x);
+    if (minstk.empty()) minstk.push(x);
+    else { int t = minstk.top(); minstk.push(min(x,t)); }
+  }
+
+  void pop() {
+    stk.pop();
+    minstk.pop();
+  }
+
+  int top() {
+    return stk.top();
+  }
+
+  int getMin() {
+    return minstk.top();
+  }
+};
+
+int main()
+{
+  MinStack slu;
+  slu.push(3);
+  cout << endl;
+  system("pause");
+}
