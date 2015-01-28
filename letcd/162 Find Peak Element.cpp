174d3b779ca87ab22414832893e0352b7a7acc98
huoyao <huoyaotl123@126.com>
2015-01-28 20:55:20 +0800

    162 Find Peak Element  --web version

---
+++ letcd/162 Find Peak Element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int findPeakElement(const vector<int> &num) {
+    int a = 0, b = num.size() - 1;
+    while (a < b)
+    {
+      int mid = (a + b) >> 1;
+      if (num[mid] < num[mid + 1]) a = mid + 1;
+      else b = mid;
+    }
+    return a;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> input = { 2, 7, 11, 15 };
+  int res = slu.findPeakElement(input);
+  cout << res << endl;
+  system("pause");
+  return 0;
+}
