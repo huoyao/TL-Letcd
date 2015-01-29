370c5c0b6f0c10cd7dffe29f12ff7ed9340e5346
huoyao <huoyaotl123@126.com>
2015-01-29 20:27:29 +0800

    168 Excel Sheet Column Title  --own version

---
+++ letcd/168 Excel Sheet Column Title.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  string convertToTitle(int n) {
+    string res;
+    --n;
+    int idx = n % 26;
+    while (idx>=0)
+    {
+      res = static_cast<char>('A'+idx)+res;
+      n /= 26;
+      --n;
+      idx = n % 26;
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  int target = 701;
+  string res = slu.convertToTitle(target);
+  cout << res << endl;
+  system("pause");
+  return 0;
+}
