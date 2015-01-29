f25accd4b768cbb54afeb49f4263b5e9e87a046b
huoyao <huoyaotl123@126.com>
2015-01-29 20:55:20 +0800

    171 Excel Sheet Column Number  --own version

---
+++ letcd/171 Excel Sheet Column Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+  int titleToNumber(string s) {
+    int len = s.length(), idx = 0, res=0;
+    while (idx < len) {res = res * 26 + s[idx++] - 'A' + 1;}
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  string str = "Z";
+  int res = slu.titleToNumber(str);
+  cout << res << endl;
+  system("pause");
+  return 0;
+}
