edc83f1621127b6ccd519517a8b88c6c416d78d5
huoyao <huoyaotl123@126.com>
2015-01-29 22:21:41 +0800

    179 Largest Number  --own version

---
+++ letcd/179 Largest Number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool cmp(const string &a, const string &b)
+{
+  string c1 = a + b, c2 = b + a;
+  return c1 > c2;
+}
+
+class Solution {
+public:
+  string largestNumber(vector<int> &num) {
+    string res = "";
+    vector<string> vecstr(num.size());
+    for (int i = 0; i < num.size(); ++i)
+      vecstr[i] = to_string(num[i]);
+    sort(vecstr.begin(), vecstr.end(), cmp);
+    for (int i = 0; i < vecstr.size(); ++i)
+      res += vecstr[i];
+    int idx = 0;
+    while (res[idx] == '0') ++idx;
+    if (idx==res.length()) return "0";  //mark
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  vector<int> input = { 0, 00 };
+  string res = slu.largestNumber(input);
+  cout << res << endl;
+  system("pause");
+  return 0;
+}
