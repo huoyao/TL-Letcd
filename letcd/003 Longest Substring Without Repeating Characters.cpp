f62181f7ea30acfdbbd304507861b4431db171ab
huoyao <huoyaotl123@126.com>
2015-01-31 21:01:40 +0800

    003 Longest Substring Without Repeating Characters  --better version

--- letcd/003 Longest Substring Without Repeating Characters.cpp
+++ letcd/003 Longest Substring Without Repeating Characters.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-    char *lptr = &s[0], *rptr = &s[0];
-    vector<bool> vectag(256, false);
-    int maxcnt = 0, cnt = 0;
-    while (*rptr!='\0')
+    vector<int> charIdx(256,-1);
+    int longest=0, maxdiff = 0,len=s.length();
+    for (int i = 0; i < len; ++i)
     {
-      if (!vectag[*rptr])
-      {
-        vectag[*rptr] = true;
-        ++cnt; 
-        ++rptr;
-      }
-      else
-      {
-        if (cnt>maxcnt) maxcnt = cnt;
-        while (*lptr != *rptr)
-        {
-          vectag[*lptr] = false;
-          ++lptr;
-          --cnt;
-        }
-        ++lptr;
-        //--cnt;   mark 1:str = "wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco";
-        ++rptr;
-      }
+      maxdiff = max(maxdiff,charIdx[s[i]]+1);
+      longest = max(longest,i-maxdiff+1);
+      charIdx[s[i]] = i;
     }
-    if (cnt>maxcnt) maxcnt = cnt;  //mark 2:str="qopubjguxhxdipfzwswybgfylqvjzhar";
-    return maxcnt;
+    return longest;
   }
 };
 
 int main()
 {
   Solution slu;
-  string str = "wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco";
+  string str = "kokok";
   int res = slu.lengthOfLongestSubstring(str);
   cout <<res<< endl;
   system("pause");
