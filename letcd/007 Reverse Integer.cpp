a18eafc597099ec70172e29db010227785782113
huoyao <huoyaotl123@126.com>
2015-02-02 20:41:13 +0800

    007 Reverse Integer   --better version --shorter

--- letcd/007 Reverse Integer.cpp
+++ letcd/007 Reverse Integer.cpp
@@ -7,32 +7,22 @@ using namespace std;
 class Solution {
 public:
   int reverse(int x) {
-    int tag = 0;
-    if (x == 0) return 0;
-    if (x < 0) tag = 1;
-    long long res = 0, maxnum = pow(2., 31) - 1;
-    string s;
-    stringstream str;
-    str << x;
-    str >> s;
-    int idx = s.length() - 1;
-    while (s[idx] == '0')--idx;
-    while (idx >= tag)
+    long long res = 0;
+    while (x)
     {
-      res = res * 10 + s[idx] - '0';
-      if (res > maxnum || tag && res > maxnum + 1) return 0;
-      --idx;
+      res = res * 10 + x%10;
+      if (res > INT_MAX || res<INT_MIN) return 0;
+      x/=10;
     }
-    int resint = res;
-    return tag ? -resint : resint;
+    return res;
   }
 };
 
 int main()
 {
   Solution slu;
-  vector<int> vecnum = { 1, 7, 11, 15 };
-  int res = slu.reverse(vecnum[0]);
+  vector<int> vecnum = { 1, 7, 11, -15 };
+  int res = slu.reverse(vecnum[3]);
   cout << res << endl;
   system("pause");
 }
