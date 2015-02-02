d4c1feee3f0140942345c3d7589276d02f5edf12
huoyao <huoyaotl123@126.com>
2015-02-02 21:59:07 +0800

    010 Regular Expression Matching  --better version --dp

--- letcd/010 Regular Expression Matching.cpp
+++ letcd/010 Regular Expression Matching.cpp
@@ -6,28 +6,28 @@ using namespace std;
 class Solution {
 public:
   bool isMatch(const char *s, const char *p) {
-    if (*p == '\0') return *s == '\0';
-    if (*(p + 1) == '*')
+    int slen = strlen(s), plen = strlen(p);
+    vector<vector<bool> > matched_dp(slen + 1, vector<bool>(plen + 1));
+    matched_dp[0][0]=true;
+    for (int i = 1; i <= slen; ++i) matched_dp[i][0] = false;
+    for (int i = 0; i < plen; ++i) matched_dp[0][i+1] = i > 0 && p[i] == '*' &&matched_dp[0][i - 1];
+    for (int i = 0; i < slen; ++i)
     {
-      while (*p == *s || (*p == '.' && *s != '\0'))
+      for (int j = 0; j < plen; ++j)
       {
-        if (isMatch(s, p + 2)) return true;
-        ++s;
+        if (p[j] != '*') matched_dp[i + 1][j + 1] = matched_dp[i][j] && (p[j] == '.' || s[i] == p[j]);
+        else matched_dp[i + 1][j + 1] = matched_dp[i + 1][j - 1] && j>0 ||
+          matched_dp[i + 1][j] || matched_dp[i][j + 1] && j>0 && (p[j - 1] == '.' || p[j - 1] == s[i]);
       }
-      return isMatch(s, p + 2);
     }
-    else
-    {
-      if (*p == *s || (*p == '.' && *s != '\0')) return isMatch(s + 1, p + 1);
-      else return false;
-    }
+    return matched_dp[slen][plen];
   }
 };
 
 int main()
 {
   Solution slu;
-  char s[] = "aaab", p[] = "a*";
+  char s[] = "aa", p[] = "aa";
   bool res = slu.isMatch(s, p);
   cout << res << endl;
   system("pause");
