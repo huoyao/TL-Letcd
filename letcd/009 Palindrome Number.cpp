3e4d12cc4eec630b8cceec1beada09e5ff8c58f3
huoyao <huoyaotl123@126.com>
2015-02-02 21:07:12 +0800

    009 Palindrome Number   --better version

--- letcd/009 Palindrome Number.cpp
+++ letcd/009 Palindrome Number.cpp
@@ -7,27 +7,20 @@ class Solution {
 public:
   bool isPalindrome(int x) {
     if (x < 0) return false;  //mark
-    if (x / 10 == 0) return true;
-    long long xid = 10;
-    while (x / xid)xid *= 10;
-    xid /= 10;
-    int iidx,jidx;
-    while (x)
+    long long reverseX = 0,bakX=x;
+    while (bakX)
     {
-      iidx = x / xid, jidx = x % 10;
-      if (iidx != jidx) return false;
-      x -= iidx*xid;
-      x /= 10;
-      xid /= 100;
+      reverseX = reverseX * 10 + bakX % 10;
+      bakX /= 10;
     }
-    return true;
+    return reverseX==x;
   }
 };
 
 int main()
 {
   Solution slu;
-  int target = 1000000001;
+  int target = 1;
   bool res = slu.isPalindrome(target);
   cout << res<< endl;
   system("pause");
