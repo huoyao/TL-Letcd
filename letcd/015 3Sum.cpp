dd3cf11ccf2d87ac64653416aa5d38ea14e05542
huoyao <huoyaotl123@126.com>
2015-02-03 11:02:43 +0800

    015 3Sum  --better version

--- letcd/015 3Sum.cpp
+++ letcd/015 3Sum.cpp
@@ -7,28 +7,23 @@ class Solution {
 public:
   vector<vector<int> > threeSum(vector<int> &num) {
     vector<vector<int> > vecres;
-    sort(num.begin(),num.end());
-    int cnt = 0;
-    for (int i = 0; i < (int)(num.size()-2); ++i)  //mark
+    sort(num.begin(), num.end());
+    int siz = num.size()-2;
+    for (int i = 0; i < siz; ++i)  //mark
     {
       if (i>0 && num[i] == num[i - 1]) continue;
-      int lidx = i + 1, ridx = num.size() - 1;
-      int sum = -num[i],sumtmp;
+      int lidx = i + 1, ridx = siz+1,sumtmp;
       while (lidx < ridx)
       {
-        sumtmp = num[lidx] + num[ridx];
-        if (sumtmp == sum)
+        sumtmp =num[i]+ num[lidx] + num[ridx];
+        if (sumtmp == 0)
         {
-          vector<int> vectmp;
-          vectmp.push_back(num[i]);
-          vectmp.push_back(num[lidx]);
-          vectmp.push_back(num[ridx]);
-          vecres.push_back(vectmp);
+          vecres.push_back({ num[i], num[lidx], num[ridx] });
           while (num[lidx + 1] == num[lidx] && lidx + 1 <= ridx) ++lidx;
           while (num[ridx - 1] == num[ridx] && ridx - 1 >= lidx) --ridx;
           ++lidx;
         }
-        else if (sumtmp > sum)
+        else if (sumtmp > 0)
           --ridx;
         else
           ++lidx;
@@ -41,9 +36,11 @@ public:
 int main()
 {
   Solution slu;
-  vector<int> vecnum = { -2, 0, 1, 1, 2 };
-  vector<vector<int> > res = slu.threeSum(vecnum);
+  vector<int> vecnum = { -2, 0, 1, 1, 2 ,4,5,6,3,4,2,5,2,1};
+  int cnt = 100;
+  vector<vector<int> > res;
+  while (cnt--)
+   res = slu.threeSum(vecnum);
   for (int i = 0; i < res.size();++i)
     cout << res[i][0] << "  " << res[i][1] << "  " << res[i][2] << endl;
-  system("pause");
 }
