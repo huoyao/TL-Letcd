9d7e686e279d6a612be207a4edb6fd89b04bc177
huoyao <huoyaotl123@126.com>
2015-02-04 19:29:38 +0800

    022 Generate Parentheses   --shorter version

--- letcd/022 Generate Parentheses.cpp
+++ letcd/022 Generate Parentheses.cpp
@@ -11,19 +11,15 @@ void getres(int l,int r,string s,vector<string> &res)
     res.push_back(s);
     return;
   }
-  if (l > 0)
-    getres(l-1,r,s+"(",res);
-  if (l<r && r>0)
-    getres(l,r-1,s+")",res);
+  if (l > 0) getres(l-1,r,s+"(",res);
+  if (l<r && r>0) getres(l,r-1,s+")",res);
 }
 
 class Solution {
 public:
   vector<string> generateParenthesis(int n) {
-    int l = n, r = n;
     vector<string> vecstr;
-    string s;
-    getres(l,r,s,vecstr);
+    getres(n,n,"",vecstr);
     return vecstr;
   }
 };
