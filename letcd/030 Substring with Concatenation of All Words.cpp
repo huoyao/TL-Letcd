b8e421013bed2699b3be14702e1d3515592492e7
huoyao <huoyaotl123@126.com>
2015-02-05 22:40:30 +0800

    030 Substring with Concatenation of All Words  --better version

--- letcd/030 Substring with Concatenation of All Words.cpp
+++ letcd/030 Substring with Concatenation of All Words.cpp
@@ -2,32 +2,55 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
   vector<int> findSubstring(string S, vector<string> &L) {
     vector<int> res;
-    if (L.size() == 0) return res;
     int slen = S.length(), llen = L.size(), strlen = L[0].length();
-    map<string, int> mapa, mapb;
+    if (llen == 0 || slen==0) return res;
+    unordered_map<string, int> mapa;
     for (int i = 0; i < L.size(); ++i)
-      mapa[L[i]]++;
-    for (int i = 0; i <= slen - llen*strlen; ++i)
+      ++mapa[L[i]];
+    for (int i = 0; i < strlen; ++i)
     {
-      mapb.clear();
-      int j = 0;
-      for (; j < llen; ++j)
+      int left = i, cnt = 0;
+      unordered_map<string, int> mapb;
+      for (int j = i; j <= slen - strlen; j+=strlen)
       {
-        string tmp = S.substr(i+j*strlen,strlen);
-        if (mapa.find(tmp) == mapa.end())
-          break;
-        mapb[tmp]++;
-        if (mapb[tmp]>mapa[tmp])
-          break;
+        string str = S.substr(j, strlen);
+        if (mapa.count(str))
+        {
+          ++mapb[str];
+          if (mapb[str] <= mapa[str])
+            ++cnt;
+          else
+          {
+            while (mapb[str] > mapa[str])
+            {
+              string strbak = S.substr(left,strlen);
+              --mapb[strbak];
+              if (mapb[strbak] < mapa[strbak]) --cnt;
+              left += strlen;
+            }
+          }
+          if (cnt == llen)
+          {
+            res.push_back(left);
+            --mapb[S.substr(left, strlen)];
+            --cnt;
+            left += strlen;
+          }
+        }
+        else
+        {
+          mapb.clear();
+          cnt = 0;
+          left = j+strlen;
+        }
       }
-      if (j == llen) res.push_back(i);
     }
     return res;
   }
@@ -36,13 +59,13 @@ public:
 int main()
 {
   Solution slu;
-  string vecnum ="abcdefg";
+  string vecnum = "abcdefg";
   vector<string> vecstr(3);
   vecstr[0] = "bc";
   vecstr[1] = "de";
   vecstr[2] = "fg";
   vector<int> res = slu.findSubstring(vecnum, vecstr);
-  for (auto &t:res)
-  cout << t << endl;
+  for (auto &t : res)
+    cout << t << endl;
   system("pause");
 }
