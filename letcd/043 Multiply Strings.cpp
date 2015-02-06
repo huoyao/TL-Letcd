52bbb7a06f92bc75ee57f738fd41f0528d997f73
huoyao <huoyaotl123@126.com>
2015-02-06 23:23:01 +0800

    043 Multiply Strings  --better version

--- letcd/043 Multiply Strings.cpp
+++ letcd/043 Multiply Strings.cpp
@@ -4,37 +4,29 @@
 #include <string>
 using namespace std;
 
-void cal(vector<int> &vecres, const int &mul, const int &idx, const vector<int> &numb, const int &len)
-{
-  int id = idx, rem = 0;
-  for (int i = 0; i < len; ++i)
-  {
-    int sum = rem + mul*numb[i];
-    vecres[id] += sum;
-    rem = vecres[id] / 10;
-    vecres[id] %= 10;
-    ++id;
-  }
-  if (rem>0)
-    vecres[id] += rem;
-}
-
 class Solution {
 public:
   string multiply(string num1, string num2) {
-    int len1 = num1.length(), len2 = num2.length();
+    int len1 = num1.length(), len2 = num2.length(), n1, n2, rem;
+    string res = "";
     if (len1 == 0 || len2 == 0 || num1[0] == '0' || num2[0] == '0') return "0";
-    vector<int> vecres(len1 + len2, 0), numa(len1), numb(len2);
+    vector<int> vecres(len1 + len2, 0);
     for (int i = 0; i < len1; ++i)
-      numa[i] = num1[len1 - i - 1] - '0';
-    for (int i = 0; i < len2; ++i)
-      numb[i] = num2[len2 - i - 1] - '0';
-    for (int i = 0; i < len1; ++i)
-      cal(vecres, numa[i], i, numb, len2);
-    string res;
-    int idx = len1+len2-1;
+    {
+      n1 = num1[len1 - i - 1] - '0';
+      rem = 0;
+      for (int j = 0; j < len2; ++j)
+      {
+        n2 = num2[len2 - j - 1] - '0';
+        int sum = n1*n2 + vecres[i + j] + rem;
+        rem = sum / 10;
+        vecres[i + j] = sum % 10;
+      }
+      if (rem>0) vecres[i + len2] += rem;
+    }
+    int idx = len1 + len2 - 1;
     while (vecres[idx] == 0)--idx;
-    while (idx >= 0) res += ('0'+vecres[idx--]);
+    while (idx >= 0) res += ('0' + vecres[idx--]);
     return res;
   }
 };
@@ -42,7 +34,7 @@ public:
 int main()
 {
   Solution slu;
-  string num1 = "3", num2 = "234";
+  string num1 = "3", num2 = "2";
   string res = slu.multiply(num1, num2);
   cout << res << endl;
   system("pause");
