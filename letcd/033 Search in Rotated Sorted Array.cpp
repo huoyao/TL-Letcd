72af75076db8d97d6b940dea74795836dbe690ea
huoyao <huoyaotl123@126.com>
2015-02-06 20:24:48 +0800

    033 Search in Rotated Sorted Array  --better version

--- letcd/033 Search in Rotated Sorted Array.cpp
+++ letcd/033 Search in Rotated Sorted Array.cpp
@@ -3,34 +3,22 @@
 #include <algorithm>
 using namespace std;
 
-int find(int a[], int l, int r, const int &target)
-{
-  if (l > r) return -1;
-  int idx = -1;
-  if (a[l] <= a[r])
-  {
-    while (l <= r)
-    {
-      int mid = (l + r) / 2;
-      if (a[mid] == target) return mid;
-      if (target>a[mid]) l = mid + 1;
-      else r = mid - 1;
-    }
-  }
-  else
-  {
-    int mid = (l + r) / 2;
-    if (a[mid] == target) return mid;
-    idx = find(a, l, mid - 1, target);
-    idx = idx == -1 ? find(a, mid + 1, r, target) : idx;
-  }
-  return idx;
-}
-
 class Solution {
 public:
   int search(int A[], int n, int target) {
-    return find(A, 0, n - 1, target);
+    int beg = 0, end = n - 1;
+    while (beg <= end)
+    {
+      int mid = (beg + end) >> 1;
+      if (A[mid] == target) return mid;
+      if (A[beg] <= A[mid])
+        if (A[beg] <= target && target < A[mid]) end = mid - 1;
+        else beg = mid + 1;
+      else
+        if (A[mid] < target && target <= A[end]) beg = mid + 1;
+        else end = mid - 1;
+    }
+    return -1;
   }
 };
 
