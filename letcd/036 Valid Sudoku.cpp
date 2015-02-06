90d96f5837169c440789e10f7ae827c3358ad78a
huoyao <huoyaotl123@126.com>
2015-02-06 21:18:18 +0800

    036 Valid Sudoku  --better version

--- letcd/036 Valid Sudoku.cpp
+++ letcd/036 Valid Sudoku.cpp
@@ -6,15 +6,16 @@ using namespace std;
 class Solution {
 public:
   bool isValidSudoku(vector<vector<char> > &board) {
-    vector<vector<bool> > row(9, vector<bool>(9, false)), col(9, vector<bool>(9, false)), block(9, vector<bool>(9, false));
+    vector<vector<bool> > col(9, vector<bool>(9, false)), block(9, vector<bool>(9, false));
     for (int i = 0; i < 9; ++i)
     {
+      vector<bool> row(9, false);
       for (int j = 0; j < 9; ++j)
       {
         if (board[i][j] == '.') continue;
-        int idx = board[i][j] - '1';
-        if (row[i][idx] || col[j][idx] || block[i - i % 3 + j / 3][idx]) return false;
-        else row[i][idx] = col[j][idx] = block[i - i % 3 + j / 3][idx] = true;
+        int idx = board[i][j] - '1', bidx = i - i % 3 + j / 3;
+        if (row[idx] || col[j][idx] || block[bidx][idx]) return false;
+        else row[idx] = col[j][idx] = block[bidx][idx] = true;
       }
     }
     return true;
