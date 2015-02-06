0538accd7645b5e45fdeda9c767d8a3c9fe5495b
huoyao <huoyaotl123@126.com>
2015-02-06 23:00:04 +0800

    042 Trapping Rain Water --better version

--- letcd/042 Trapping Rain Water.cpp
+++ letcd/042 Trapping Rain Water.cpp
@@ -3,37 +3,33 @@
 #include <algorithm>
 using namespace std;
 
-void water(int &sum,const int A[],const int &n,int &i)
-{
-  int curidx = i, maxidx = i+1, lastid = i+1,curval=A[i],tmp=0;
-  while (lastid < n && A[lastid] < curval)
-  {
-    if (A[lastid]>A[maxidx]) maxidx = lastid;
-    ++lastid;
-  }
-  if (lastid < n) maxidx = lastid;
-  int val = min(curval,A[maxidx]);
-  for (int idx = i + 1; idx < maxidx; ++idx)
-    tmp += (val-A[idx]);
-  sum +=tmp;
-  i = maxidx;
-}
-
 class Solution {
 public:
   int trap(int A[], int n) {
-    if (n < 2) return 0;
-    int sum = 0;
-    int i = 0;
-    while (i < n - 1) water(sum, A, n, i);
-    return sum;
+    int lmax = 0, left = 0, rmax = 0, right = n - 1.res = 0;
+    while (left <= right)
+    {
+      if (A[left] <= A[right])
+      {
+        if (A[left] >= lmax) lmax = A[left];
+        else res += maxleft - A[left];
+        ++left;
+      }
+      else
+      {
+        if (A[right] >= rmax) rmax = A[right];
+        else res += rmax - A[right];
+        ++right;
+      }
+    }
+    return res;
   }
 };
 
 int main()
 {
   Solution slu;
-  int input[] = { 2,0, 7, 0, 3 };
+  int input[] = { 2, 0, 7, 0, 3 };
   int target = 5;
   int res = slu.trap(input, target);
   cout << res << endl;
