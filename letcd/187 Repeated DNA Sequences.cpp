a08646c42ecfa71c1b527ba5581421d59f10efbb
huoyao <huoyaotl123@126.com>
2015-02-06 19:43:08 +0800

    187 Repeated DNA Sequences  --web version

---
+++ letcd/187 Repeated DNA Sequences.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+  vector<string> findRepeatedDnaSequences(string s) {
+    vector<string> res;
+    unordered_map<int, int> mapidx;
+    int win=0, len = s.length(),idx=0;
+    while (idx < 9)
+      win = win << 3 | s[idx++] & 7;
+    while (idx<len)
+      if (mapidx[win = win << 3 & 0x3FFFFFFF | s[idx++] & 7]++ == 1)
+        res.push_back(s.substr(idx-10,10));
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  string ss = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+  vector<string> res = slu.findRepeatedDnaSequences(ss);
+  for (auto &t : res)
+    cout << t << endl;
+  system("pause");
+}
