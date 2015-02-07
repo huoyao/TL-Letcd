8adeb95bc1b0b103f2620056f44644ef37c26a4c
huoyao <huoyaotl123@126.com>
2015-02-07 23:22:19 +0800

    072 Edit Distance  --better version

--- letcd/072 Edit Distance.cpp
+++ letcd/072 Edit Distance.cpp
@@ -6,20 +6,23 @@ using namespace std;
 class Solution {
 public:
   int minDistance(string word1, string word2) {
-    int len1 = word1.length() + 1, len2 = word2.length() + 1;
-    vector<vector<int> > vec(len1,vector<int>(len2));
-    for (int i = 0; i < len1; ++i) vec[i][0] = i;
-    for (int j = 0; j < len2; ++j) vec[0][j] = j;
-    for (int i = 1; i < len1; ++i)
+    int len1 = word1.length(), len2 = word2.length(),pre,cur;
+    vector<int> dp(len2+1);
+    for (int i = 0; i <= len2; ++i)
+      dp[i] = i;
+    for (int i = 1; i <= len1; ++i)
     {
-      for (int j = 1; j < len2; ++j)
+      pre = i;
+      for (int j = 1; j <= len2; ++j)
       {
-        if (word1[i - 1] == word2[j - 1]) vec[i][j] = vec[i - 1][j - 1];
-        else vec[i][j] = vec[i - 1][j - 1] + 1;
-        vec[i][j] = min(vec[i][j],min(vec[i-1][j]+1,vec[i][j-1]+1));
+        if (word1[i - 1] == word2[j - 1]) cur= dp[j-1];
+        else cur=min(pre,min(dp[j],dp[j-1]))+1;
+        dp[j - 1] = pre;
+        pre = cur;
       }
+      dp[len2] = pre;
     }
-    return vec[len1 - 1][len2 - 1];
+    return dp[len2];
   }
 };
 
