f59ca9009678fabcde9050d0a50bdd615ecdba39
huoyao <huoyaotl123@126.com>
2015-02-07 14:42:10 +0800

    054 Spiral Matrix  --better version

--- letcd/054 Spiral Matrix.cpp
+++ letcd/054 Spiral Matrix.cpp
@@ -6,27 +6,26 @@ using namespace std;
 class Solution {
 public:
   vector<int> spiralOrder(vector<vector<int> > &matrix) {
-    int m = matrix.size(), n = matrix.size()?matrix[0].size():0;   //mark
-    int minelem = min(m, n);
-    int id = minelem / 2, round1 = n - 1, round2 = m - 1, i = 0;
+    int m = matrix.size(), n = matrix.size()?matrix[0].size():0,cnt=0;   //mark
+    int minelem = min(m, n),id = minelem / 2, round1 = n - 1, round2 = m - 1, i = 0;
     bool moreline = false;
-    vector<int> res;
+    vector<int> res(m*n);
     if (minelem % 2) moreline = true;
     for (; i < id; ++i)
     {
       int idx1 = i, idx2 = i;
-      while (idx1 < round1) res.push_back(matrix[i][idx1++]);
-      while (idx2 < round2) res.push_back(matrix[idx2++][idx1]);
-      while (idx1 > i) res.push_back(matrix[idx2][idx1--]);
-      while (idx2 > i) res.push_back(matrix[idx2--][idx1]);
+      while (idx1 < round1) res[cnt++]=matrix[i][idx1++];
+      while (idx2 < round2) res[cnt++]=matrix[idx2++][idx1];
+      while (idx1 > i) res[cnt++]=matrix[idx2][idx1--];
+      while (idx2 > i) res[cnt++]=matrix[idx2--][idx1];
       --round1;
       --round2;
     }
     if (!moreline) return res;
     if (minelem == m)
-      for (int j = i; j <= round1; ++j) res.push_back(matrix[i][j]);
+      for (int j = i; j <= round1; ++j) res[cnt++]=matrix[i][j];
     else
-      for (int j = i; j <= round2; ++j) res.push_back(matrix[j][i]);
+      for (int j = i; j <= round2; ++j) res[cnt++]=matrix[j][i];
     return res;
   }
 };
