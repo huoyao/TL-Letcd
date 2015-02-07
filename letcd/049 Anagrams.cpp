d192f7d630858a35cfc2d24ccb73920e2361a3a1
huoyao <huoyaotl123@126.com>
2015-02-07 13:51:28 +0800

    049 Anagrams  --little change

--- letcd/049 Anagrams.cpp
+++ letcd/049 Anagrams.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
   vector<string> anagrams(vector<string> &strs) {
-    map<string, string> mapt;
+    unordered_map<string, string> mapt;
     string target;
     vector<string> vecres;
     for (int i = 0; i < strs.size(); ++i)
@@ -20,8 +20,10 @@ public:
       else
       {
         if (mapt[tmp] != "A")
+        {
           vecres.push_back(mapt[tmp]);
-        mapt[tmp] = "A";
+          mapt[tmp] = "A";
+        }
         vecres.push_back(strs[i]);
       }
     }
