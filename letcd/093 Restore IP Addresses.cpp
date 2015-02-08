13644cb7f412945bd3a8ee7b18fe9f25cfa1c3fc
huoyao <huoyaotl123@126.com>
2015-02-08 19:41:07 +0800

    093 Restore IP Addresses  --better version

--- letcd/093 Restore IP Addresses.cpp
+++ letcd/093 Restore IP Addresses.cpp
@@ -4,34 +4,35 @@
 #include <string>
 using namespace std;
 
-void findIP(vector<string> &res, vector<string> rec, const string &s, int idx, const int &len)
-{
-  if (rec.size() == 4)
-  {
-    string tmp = rec[0] + "." + rec[1] + "." + rec[2] + "." + rec[3];
-    if (idx == len) res.push_back(tmp);
-    return;
-  }
-  int val = 0;
-  for (int i = 0; i < 3 && idx+i<len; ++i)
-  {
-    if (val == 0 && i>0) return; //mark
-    val = val * 10 + s[idx + i] - '0';
-    if (val>255) return;
-    rec.push_back(s.substr(idx,i+1));
-    findIP(res,rec,s,idx+i+1,len);
-    rec.pop_back();
-  }
-}
-
 class Solution {
+private:
+  vector<string> res;
+  vector<string> rec;
+  int len;
 public:
   vector<string> restoreIpAddresses(string s) {
-    int len = s.length();
-    vector<string> res;
-    vector<string> rec;
-    findIP(res,rec,s,0,len);
+    len = s.length();
+    findIP(s,0);
     return res;
+  }
+  void findIP(const string &s, int idx)
+  {
+    if (rec.size() == 4)
+    {
+      string tmp = rec[0] + "." + rec[1] + "." + rec[2] + "." + rec[3];
+      if (idx == len) res.push_back(tmp);
+      return;
+    }
+    int val = 0;
+    for (int i = 0; i < 3 && idx + i<len; ++i)
+    {
+      if (val == 0 && i>0) return; //mark
+      val = val * 10 + s[idx + i] - '0';
+      if (val>255) return;
+      rec.push_back(s.substr(idx, i + 1));
+      findIP(s, idx + i + 1);
+      rec.pop_back();
+    }
   }
 };
 
