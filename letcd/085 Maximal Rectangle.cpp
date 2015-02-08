daa0338dff36a55b4fd78c6ce185f7378502a4a1
huoyao <huoyaotl123@126.com>
2015-02-08 17:01:08 +0800

    085 Maximal Rectangle  --better version

--- letcd/085 Maximal Rectangle.cpp
+++ letcd/085 Maximal Rectangle.cpp
@@ -4,39 +4,39 @@
 #include <stack>
 using namespace std;
 
-int hist(const vector<int> &ht)
-{
-  int res = 0;
-  stack<int> st;
-  for (int i = 0; i < ht.size();)
-  {
-    if (st.empty() || ht[st.top()] < ht[i]) st.push(i++);
-    else
-    {
-      int idx = st.top();
-      st.pop();
-      res = max(res,(st.empty()?i:i-st.top()-1)*ht[idx]);
-    }
-  }
-  return res;
-}
-
 class Solution {
 public:
   int maximalRectangle(vector<vector<char> > &matrix) {
     if (matrix.size() == 0 || matrix[0].size() == 0) return 0;
     vector<int> ht(matrix[0].size()+1,0);
     int maxres = 0;
+    bool needupdate = false;
     for (int i = 0; i < matrix.size(); ++i)
     {
       for (int j = 0; j < matrix[i].size(); ++j)
       {
-        if (matrix[i][j] == '1') ++ht[j];
+        if (matrix[i][j] == '1') { ++ht[j]; needupdate = true; }
         else ht[j] = 0;
       }
-      maxres = max(maxres, hist(ht));
+      if (needupdate) maxres = max(maxres, hist(ht));
     }
     return maxres;
+  }
+  int hist(const vector<int> &ht)
+  {
+    int res = 0;
+    stack<int> st;
+    for (int i = 0; i < ht.size();)
+    {
+      if (st.empty() || ht[st.top()] < ht[i]) st.push(i++);
+      else
+      {
+        int idx = st.top();
+        st.pop();
+        res = max(res, (st.empty() ? i : i - st.top() - 1)*ht[idx]);
+      }
+    }
+    return res;
   }
 };
 
