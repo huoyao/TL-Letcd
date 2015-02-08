4fa8e109020c8e5caff0fcf9190bfe449cfb2db4
huoyao <huoyaotl123@126.com>
2015-02-08 20:18:04 +0800

    098 Validate Binary Search Tree  --better version

--- letcd/098 Validate Binary Search Tree.cpp
+++ letcd/098 Validate Binary Search Tree.cpp
@@ -11,21 +11,19 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-bool checkValid(TreeNode *root,const int min_e,const int max_e,int &cntmin,int &cntmax)
-{
-  if (!root) return true;
-  if (root->val == INT_MIN) ++cntmin;
-  if (root->val == INT_MAX) ++cntmax;
-  return (root->val > min_e || min_e==INT_MIN && root->val == INT_MIN && cntmin == 1) && (root->val < max_e || INT_MAX==max_e && root->val == INT_MAX &&cntmax == 1)
-    && checkValid(root->left, min_e, root->val,cntmin,cntmax) && checkValid(root->right, root->val, max_e,cntmin,cntmax);
-}
-
 class Solution {
 public:
   bool isValidBST(TreeNode *root) {
-    if (!root || !root->left && !root->right) return true;
-    int cntmin = 0, cntmax = 0;
-    return checkValid(root,INT_MIN,INT_MAX,cntmin,cntmax);
+    TreeNode *pre = NULL;
+    return isvalid(root,pre);
+  }
+  bool isvalid(TreeNode *root,TreeNode *&pre)
+  {
+    if (!root) return true;
+    if (!isvalid(root->left, pre)) return false;
+    if (pre && pre->val >= root->val) return false;
+    pre = root;
+    return isvalid(root->right,pre);
   }
 };
 
