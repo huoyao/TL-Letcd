14f63ab3a66fa356844a3cc34740c12a63afff9a
huoyao <huoyaotl123@126.com>
2015-02-08 19:35:10 +0800

    091 Decode Ways  --better version

--- letcd/091 Decode Ways.cpp
+++ letcd/091 Decode Ways.cpp
@@ -9,23 +9,21 @@ public:
   int numDecodings(string s) {
     int len = s.length();
     if (len == 0) return 0;
+    if (s[0] == '0') return 0;
     vector<int> dp(len, 0);
-    for (int i = 0; i < len; ++i)
+    dp[0] = 1;
+    if (len > 1)
     {
-      if (s[i] >= '1' && s[i] <= '9')
-      {
-        if (i == 0) dp[i] = 1;
-        else dp[i] += dp[i - 1];
-      }
-      if (i == 0) continue;
-      string tmp(s, i - 1, 2);
-      if (tmp >= "10" && tmp <= "26")
-      {
-        if (i > 1) dp[i] += dp[i - 2];
-        else dp[i] += 1;
-      }
+      dp[1] += s[1] != '0' ? 1 : 0;
+      dp[1] += s.substr(0, 2) >= "10" && s.substr(0, 2) <= "26" ? 1 : 0;
     }
-    return dp[len-1];
+    for (int i = 2; i < len; ++i)
+    {
+      dp[i] += s[i] != '0' ? dp[i - 1] : 0;
+      string tmp(s, i - 1, 2);
+      dp[i] += tmp >= "10" && tmp <= "26" ? dp[i - 2] : 0;
+    }
+    return dp[len - 1];
   }
 };
 
