3a0e2f57a255f63a6144dade35c62e4be434f331
huoyao <huoyaotl123@126.com>
2015-02-08 16:04:58 +0800

    018 4Sum  --format change

--- letcd/018 4Sum.cpp
+++ letcd/018 4Sum.cpp
@@ -6,25 +6,11 @@
 #include <iterator>
 using namespace std;
 
-void addp(pair<int, int> &par1, pair<int, int> &par2, vector<vector<int> > &res, vector<int> &num)
-{
-  int &a1 = par1.first, &a2 = par1.second, &b1 = par2.first, &b2 = par2.second;
-  if (b1<=a2 || b2==a1 || a1 == b1 || a2 == b2 ) return;
-  if (res.size() == 0) res.push_back({ num[a1], num[a2], num[b1], num[b2] });
-  vector<int> vec = res.back(), vec1 = { num[a1], num[a2], num[b1], num[b2] };
-  if (vec==vec1) return;
-  res.push_back(vec1);
-}
-
-bool chk(vector<int> &num,vector<pair<int, int> > &vecp, const int &i)
-{
-  return i && num[vecp[i].first] == num[vecp[i - 1].first] && num[vecp[i].second] == num[vecp[i - 1].second];
-}
-
 class Solution {
+private:
+  vector<vector<int> >res;
 public:
   vector<vector<int> > fourSum(vector<int> &num, int target) {
-    vector<vector<int> >res;
     if (num.size() < 4) return res;
     unordered_map<int, vector<pair<int, int> > > mapsum;
     vector<int> vecsum;
@@ -54,7 +40,7 @@ public:
           if (chk(num,vecpl, i)) continue;
           vector<pair<int, int> > &vecpr = mapsum[vecsum[ridx]];
           for (int j = 0; j < vecpr.size(); ++j)
-            addp(vecpl[i], vecpr[j], res, num);
+            addp(vecpl[i], vecpr[j], num);
         }
         ++lidx;
         --ridx;
@@ -66,9 +52,22 @@ public:
     {
       if (chk(num,vecpl,i)) continue;
       for (int j = i + 1; j <vecpl.size(); ++j)
-        addp(vecpl[i], vecpl[j], res, num);
+        addp(vecpl[i], vecpl[j], num);
     }
     return res;
+  }
+  void addp(pair<int, int> &par1, pair<int, int> &par2, vector<int> &num)
+  {
+    int &a1 = par1.first, &a2 = par1.second, &b1 = par2.first, &b2 = par2.second;
+    if (b1 <= a2 || b2 == a1 || a1 == b1 || a2 == b2) return;
+    if (res.size() == 0) res.push_back({ num[a1], num[a2], num[b1], num[b2] });
+    vector<int> vec = res.back(), vec1 = { num[a1], num[a2], num[b1], num[b2] };
+    if (vec == vec1) return;
+    res.push_back(vec1);
+  }
+  bool chk(vector<int> &num, vector<pair<int, int> > &vecp, const int &i)
+  {
+    return i && num[vecp[i].first] == num[vecp[i - 1].first] && num[vecp[i].second] == num[vecp[i - 1].second];
   }
 };
 
