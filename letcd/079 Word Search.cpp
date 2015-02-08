312fb8df41314241e7a83fc08a6640a8332cb6ec
huoyao <huoyaotl123@126.com>
2015-02-08 16:18:15 +0800

    079 Word Search  --better version

--- letcd/079 Word Search.cpp
+++ letcd/079 Word Search.cpp
@@ -3,28 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-bool find(const int &i, const int &j, const string &wd, vector<vector<char> > &board,int k)
-{
-  if (k == wd.size())
-    return true;
-  if (i >= 0 && i < board.size() && j >= 0 && j < board[i].size() && board[i][j] == wd[k])
-  {
-    char tmp = board[i][j];
-    board[i][j] = '*';
-    bool up = find(i+1, j, wd, board, k+1);
-    bool down = find(i - 1, j, wd, board, k + 1);
-    bool left = find(i, j-1, wd, board, k + 1);
-    bool right = find(i , j+1, wd, board, k + 1);
-    if (up || down || left || right) return true;
-    else
-    {
-      board[i][j] = tmp;
-      return false;
-    }
-  }
-  return false;
-}
-
 class Solution {
 public:
   bool exist(vector<vector<char> > &board, string word) {
@@ -35,6 +13,27 @@ public:
       {
         bool flg=find(i, j, word, board,0);
         if (flg == true) return flg;
+      }
+    }
+    return false;
+  }
+  bool find(const int &i, const int &j, const string &wd, vector<vector<char> > &board, int k)
+  {
+    if (k == wd.size())
+      return true;
+    if (i >= 0 && i < board.size() && j >= 0 && j < board[i].size() && board[i][j] == wd[k])
+    {
+      char tmp = board[i][j];
+      board[i][j] = '*';
+      bool up = find(i + 1, j, wd, board, k + 1);
+      bool down = find(i - 1, j, wd, board, k + 1);
+      bool left = find(i, j - 1, wd, board, k + 1);
+      bool right = find(i, j + 1, wd, board, k + 1);
+      if (up || down || left || right) return true;
+      else
+      {
+        board[i][j] = tmp;
+        return false;
       }
     }
     return false;
