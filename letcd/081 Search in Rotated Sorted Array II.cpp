e87f49958cd38914d15766514f3e3a4a90720254
huoyao <huoyaotl123@126.com>
2015-02-08 16:26:22 +0800

    081 Search in Rotated Sorted Array II  --better version

--- letcd/081 Search in Rotated Sorted Array II.cpp
+++ letcd/081 Search in Rotated Sorted Array II.cpp
@@ -3,34 +3,33 @@
 #include <algorithm>
 using namespace std;
 
-bool bisech(int start, int end, int A[], const int &target)
-{
-  if (start > end) return false;
-  if (start == end) return A[start] == target;
-  if (A[start] < A[end])
-  {
-    while (start <= end)
-    {
-      int mid = (start + end) >> 1;
-      if (A[mid] == target) return true;
-      if (A[mid] > target) end = mid - 1;
-      else start = mid + 1;
-    }
-    return false;
-  }
-  else
-  {
-    int mid = (start + end) >> 1;
-    if (A[mid] == target) return true;
-    return bisech(start, mid-1, A, target) || bisech(mid+1, end, A, target);
-  }
-}
-
 class Solution {
 public:
   bool search(int A[], int n, int target) {
     if (n <= 0) return false;
     return bisech(0, n - 1, A, target);
+  }
+  bool bisech(int start, int end, int A[], const int &target)
+  {
+    if (start > end) return false;
+    if (start == end) return A[start] == target;
+    if (A[start] < A[end])
+    {
+      while (start <= end)
+      {
+        int mid = (start + end) >> 1;
+        if (A[mid] == target) return true;
+        if (A[mid] > target) end = mid - 1;
+        else start = mid + 1;
+      }
+      return false;
+    }
+    else
+    {
+      int mid = (start + end) >> 1;
+      if (A[mid] == target) return true;
+      return bisech(start, mid - 1, A, target) || bisech(mid + 1, end, A, target);
+    }
   }
 };
 
