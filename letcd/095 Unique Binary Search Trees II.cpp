4852b16b634f99d2ae5481aa8529d5379e8137ca
huoyao <huoyaotl123@126.com>
2015-02-08 19:44:16 +0800

    095 Unique Binary Search Trees II  --bette version

--- letcd/095 Unique Binary Search Trees II.cpp
+++ letcd/095 Unique Binary Search Trees II.cpp
@@ -11,36 +11,35 @@ struct TreeNode {
   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-vector<TreeNode *> build(int start,int end)
-{
-  vector<TreeNode *> res;
-  if (start > end)
-  {
-    res.push_back(NULL);
-    return res;
-  }
-  for (int k = start; k <= end; ++k)
-  {
-    vector<TreeNode *> left = build(start,k-1);
-    vector<TreeNode *> right = build(k+1,end);
-    for (int i = 0; i < left.size(); ++i)
-    {
-      for (int j = 0; j < right.size(); ++j)
-      {
-        TreeNode *root = new TreeNode(k);
-        root->left = left[i];
-        root->right = right[j];
-        res.push_back(root);
-      }
-    }
-  }
-  return res;
-}
-
 class Solution {
 public:
   vector<TreeNode *> generateTrees(int n) {
     return build(1,n);
+  }
+  vector<TreeNode *> build(int start, int end)
+  {
+    vector<TreeNode *> res;
+    if (start > end)
+    {
+      res.push_back(NULL);
+      return res;
+    }
+    for (int k = start; k <= end; ++k)
+    {
+      vector<TreeNode *> left = build(start, k - 1);
+      vector<TreeNode *> right = build(k + 1, end);
+      for (int i = 0; i < left.size(); ++i)
+      {
+        for (int j = 0; j < right.size(); ++j)
+        {
+          TreeNode *root = new TreeNode(k);
+          root->left = left[i];
+          root->right = right[j];
+          res.push_back(root);
+        }
+      }
+    }
+    return res;
   }
 };
 
