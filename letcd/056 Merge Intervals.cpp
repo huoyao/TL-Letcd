efd0e6db1687e0a9090e8c74139aec94b6f2a9a4
huoyao <huoyaotl123@126.com>
2015-02-08 14:02:59 +0800

    056 Merge Intervals  --format change

--- letcd/056 Merge Intervals.cpp
+++ letcd/056 Merge Intervals.cpp
@@ -11,19 +11,12 @@ struct Interval {
   Interval(int s, int e) : start(s), end(e) {}
 };
 
-bool cmp(const Interval &t1,const Interval &t2)
-{
-  if (t1.start == t2.start)
-    return t1.end < t2.end;
-  return t1.start < t2.start;
-}
-
 class Solution {
 public:
   vector<Interval> merge(vector<Interval> &intervals) {
     vector<Interval> res;
     if (intervals.size() <= 0) return res;
-    sort(intervals.begin(),intervals.end(),cmp);
+    sort(intervals.begin(), intervals.end(), cmp);
     Interval tmp(intervals[0].start,intervals[0].end);
     for (int i=1; i < intervals.size(); ++i)
     {
@@ -37,6 +30,12 @@ public:
     }
     res.push_back(tmp);
     return res;
+  }
+  static bool cmp(const Interval &t1, const Interval &t2)
+  {
+    if (t1.start == t2.start)
+      return t1.end < t2.end;
+    return t1.start < t2.start;
   }
 };
 
