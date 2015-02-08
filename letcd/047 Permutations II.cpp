e48d30e29bfe6434ae17a6c0ca69af8560bbdd2a
huoyao <huoyaotl123@126.com>
2015-02-08 15:25:21 +0800

    047 Permutations II  --better version

--- letcd/047 Permutations II.cpp
+++ letcd/047 Permutations II.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-void find(const vector<int> &num, vector<int> &rec, vector<bool> &notuse, vector<vector<int> > &res)
-{
-  if (rec.size() == num.size())
-  {
-    res.push_back(rec);
-    return;
-  }
-  for (int i = 0; i < num.size(); ++i)
-  {
-    if (i != 0 && num[i] == num[i - 1] && notuse[i - 1]) continue;
-    if (!notuse[i]) continue;
-    rec.push_back(num[i]);
-    notuse[i] = false;
-    find(num, rec, notuse, res);
-    rec.pop_back();
-    notuse[i] = true;
-  }
-}
-
 class Solution {
+private:
+  vector<vector<int> > res;
+  vector<int> rec;
+  vector<bool> notuse;
+  int len;
 public:
   vector<vector<int> > permuteUnique(vector<int> &num) {
-    vector<int> record;
-    vector<vector<int> > res;
-    if (num.size() <= 1) { res.push_back(num); return res; }
+    len = num.size();
+    notuse = vector<bool>(len,true);
+    if (len <= 1) { res.push_back(num); return res; }
     sort(num.begin(), num.end());
-    vector<bool> notused(num.size(), true);
-    find(num, record, notused, res);
+    find(num,0);
     return res;
+  }
+  void find(vector<int> num, int beg)
+  {
+    if (beg >= num.size())
+    {
+      res.push_back(num);
+      return;
+    }
+    unordered_set<int> diff;
+    for (int i = beg; i < num.size(); ++i)
+    {
+      if (diff.count(num[i])) continue;
+      else diff.insert(num[i]);
+      swap(num[beg], num[i]);
+      find(num, beg + 1);
+      swap(num[beg], num[i]);
+    }
   }
 };
 
 int main()
 {
   Solution slu;
-  vector<int> input = { 1, 1 };
-  vector<vector<int> > res = slu.permute(input);
+  vector<int> input = { 0, 1,0, 1};
+  vector<vector<int> > res = slu.permuteUnique(input);
   for (auto &t : res)
   {
     for (auto &k : t)
