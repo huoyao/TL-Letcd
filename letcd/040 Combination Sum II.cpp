6c18cf6e2ac9c871a6aa3d3c5e418ec24cbbe721
huoyao <huoyaotl123@126.com>
2015-02-08 15:29:05 +0800

    040 Combination Sum II  --better version

--- letcd/040 Combination Sum II.cpp
+++ letcd/040 Combination Sum II.cpp
@@ -3,31 +3,31 @@
 #include <algorithm>
 using namespace std;
 
-void combi(const vector<int> &cand, vector<vector<int> > &res, vector<int> record, int idx, int sum, const int &target)
-{
-  if (sum == target)
-  {
-    res.push_back(record);
-    return;
-  }
-  for (int i = idx; i < cand.size(); ++i)
-  {
-    if (sum + cand[i] > target) break;
-    if (i && cand[i] == cand[i - 1] && i > idx) continue;
-    record.push_back(cand[i]);
-    combi(cand, res, record, i+1, sum + cand[i], target);
-    record.pop_back();
-  }
-}
-
 class Solution {
+private:
+  vector<vector<int> > res;
+  vector<int> record;
 public:
   vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
     sort(candidates.begin(), candidates.end());
-    vector<vector<int> > res;
-    vector<int> record;
-    combi(candidates, res, record, 0, 0, target);
+    combi(candidates, 0, 0, target);
     return res;
+  }
+  void combi(const vector<int> &cand, int idx, int sum, const int &target)
+  {
+    if (sum == target)
+    {
+      res.push_back(record);
+      return;
+    }
+    for (int i = idx; i < cand.size(); ++i)
+    {
+      if (sum + cand[i] > target) break;
+      if (i && cand[i] == cand[i - 1] && i > idx) continue;
+      record.push_back(cand[i]);
+      combi(cand, i + 1, sum + cand[i], target);
+      record.pop_back();
+    }
   }
 };
 
