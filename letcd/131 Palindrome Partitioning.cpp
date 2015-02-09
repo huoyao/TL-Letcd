5260b9dae7ccacf119282120f676c6f9cbd3a71b
huoyao <huoyaotl123@126.com>
2015-02-09 23:25:05 +0800

    131 Palindrome Partitioning  --better version

--- letcd/131 Palindrome Partitioning.cpp
+++ letcd/131 Palindrome Partitioning.cpp
@@ -4,39 +4,43 @@
 #include <string>
 using namespace std;
 
-void dfs(string s, vector<string> rec, vector<vector<string> > &res)
-{
-  if (s.length() == 0)
-  {
-    res.push_back(rec);
-    return;
-  }
-  for (int i = 0; i < s.length(); ++i)
-  {
-    int beg = 0, end = i;
-    while (beg < end)
-    {
-      if (s[beg] != s[end]) break;
-      ++beg;
-      --end;
-    }
-    if (beg >= end)
-    {
-      rec.push_back(s.substr(0,i+1));
-      dfs(s.substr(i+1),rec,res);
-      rec.pop_back();
-    }
-  }
-}
-
 class Solution {
+private:
+  vector<vector<string> > res;
+  vector<string> rec;
+  vector<vector<int> > map_pali;
 public:
   vector<vector<string>> partition(string s) {
-    vector<vector<string> > res;
+    int n = s.length();
     if (s.length() == 0) return res;
-    vector<string> rec;
-    dfs(s,rec,res);
+    vector<vector<bool> > map = vector<vector<bool> >(n, vector<bool>(n, false));
+    map_pali.resize(n);
+    for (int i = n - 1; i >= 0; --i)
+    {
+      for (int j = i; j < n; ++j)
+      {
+        map[i][j] = s[i] == s[j] && (j - i < 2 || map[i + 1][j - 1]);
+        if (map[i][j]) map_pali[i].push_back(j);
+      }
+    }
+    dfs(s,0);
     return res;
+  }
+  void dfs(string s, int end)
+  {
+    if (s.length() == end)
+    {
+      res.push_back(rec);
+      return;
+    }
+    for (int i = 0; i < map_pali[end].size(); ++i)
+    {
+      int endidx=map_pali[end][i]+1;
+      if (endidx<s.length() && map_pali[endidx].size() == 0) continue;
+      rec.push_back(s.substr(end,endidx-end));
+      dfs(s,endidx);
+      rec.pop_back();
+    }
   }
 };
 
