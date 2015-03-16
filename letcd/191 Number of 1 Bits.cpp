588af1cff6daec292d41a102eb01d25be74b682f
huoyao <huoyaotl123@126.com>
2015-03-16 23:13:12 +0800

    191 Number of 1 Bits

---
+++ letcd/191 Number of 1 Bits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+class Solution
+{
+public:
+  int hammingWeight(uint32_t n) {
+    int res = 0;
+    while(n)
+    {
+      n &= n - 1;
+      ++res;
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  Solution slu;
+  uint32_t input = 1;
+  int res = slu.hammingWeight(input);
+    cout << res << "  ";
+  cout << endl;
+  system("pause");
+  return 0;
+}
